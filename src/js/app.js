f32e6888b50c0c06bff9b51d47d5c26cfcb9d6ab
Nadsadin <leloushlampergh@gmail.com>
2020-03-06 11:19:44 +0300

    update navbar, add menu

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,15 @@ import {MDCTextField} from '@material/textfield';
 $(function() {
     $(document).on('click', '.anchor-link', function(e) {
         e.preventDefault();
-        // $('#navbar-collapse').collapse('hide');
+        $('#navbar-collapse').collapse('hide');
         $("html, body").stop().animate({
             scrollTop: Math.round($(this.getAttribute('href')).offset().top-70) + 'px'
         }, 1500);
     });
     $(window).scroll(function(){
-        $('.navbar.fixed-top').toggleClass('bg-white navbar-light', $(this).scrollTop() > 20);
+        $('.navbar.fixed-top').toggleClass('bg-white', $(this).scrollTop() > 20);
     });
-    $('.navbar.fixed-top').toggleClass('bg-white navbar-light', $(this).scrollTop() > 20);
+    $('.navbar.fixed-top').toggleClass('bg-white', $(this).scrollTop() > 20);
     const text_fields = document.querySelectorAll('.mdc-text-field');
     for (const text_field of text_fields) {
         new MDCTextField(text_field)
